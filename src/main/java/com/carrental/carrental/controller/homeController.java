755b377ae8ce2ec33c96751138f5abf71d6b09c0
7thCoder <nuibima@gmail.com>
2024-01-19 11:31:46 +0100

    wrote test for home controller

--- src/main/java/com/carrental/carrental/controller/homeController.java
+++ src/main/java/com/carrental/carrental/controller/homeController.java
@@ -1,9 +1,18 @@
 package com.carrental.carrental.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Controller
-@RequestMapping ("/", "/home")
-public class homeController {
-    
+@Configuration
+@RequestMapping("/")
+public class homeController implements WebMvcConfigurer {
+   
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login");
+
+    }
 }
